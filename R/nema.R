c9dd42390611157741481b9957824cf6d4183e31
chihlinwei <chihlinwei@gmail.com>
2018-06-30 09:48:36 +0800

    update

---
+++ R/nema.R
@@ -0,0 +1,7 @@
+#' Package for analyzing nematode community structure
+#'
+#' swtmap package contains simple tools to generate bathymetric map off SW Taiwan.
+#'
+#' @author Chih-Lin Wei <chihlinwei@@gmail.com>, Jian-Xiang Liao <jianxiangliao@@gmail.com >
+#' @name nema
+NULL
